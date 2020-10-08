cb87f2a20a4758db46910aecb7694d08c53af745
amp5 <aplassaras@gmail.com>
2020-10-08 12:10:38 -0700

    returns json for single plant

--- server/access_data.py
+++ server/access_data.py
@@ -1,6 +1,8 @@
 import configparser
 import requests
 import pandas as pd
+
+# Useful for debugging only
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 
@@ -36,28 +38,52 @@ class PlantAPI:
         )
         
         response = response.json()
-        # Dict keys = data, links, meta
-        # print(response.keys())
         df = pd.DataFrame(response['data'])
         return df
 
-    def get_indiv_plant(self):
-        response = requests.get('https://trefle.io/api/v1/species/158178?token=' + self.access_token)
+    def get_indiv_plant(self, species_id):
+        response = requests.get('https://trefle.io/api/v1/species/'+ species_id + '?token=' + self.access_token)
         response = response.json()
-        # Dict keys = data, links, meta
-        print(response.keys())
-        df = response['data']
+
+        df = pd.DataFrame.from_dict(response, orient='index')
+        df.reset_index(level=0, inplace=True)
         return df
+
+    def transform(self, plant_obj):
+        df_plant = plant_obj[['id', 'common_name',
+                          'scientific_name', 'image_url',
+                          'vegetable', 'edible_part',
+                          'edible', 'images',
+                          'flower', 'foliage',
+                          'fruit_or_seed', 'specifications'
+                          ]].copy()
+        indiv_plant = df_plant.iloc[0]
+        indiv_plant['flower_color'] = indiv_plant['flower']['color']
+        indiv_plant['foliage_color'] = indiv_plant['foliage']['color']
+        indiv_plant['fruit_or_seed_color'] = indiv_plant['fruit_or_seed']['color']
+        indiv_plant['toxicity'] = indiv_plant['specifications']['toxicity']
+        del indiv_plant['flower']
+        del indiv_plant['foliage']
+        del indiv_plant['specifications']
+        del indiv_plant['fruit_or_seed']
+
+        return indiv_plant
+
+    def json_result(self, pd_df):
+        result = pd_df.to_json()
+        return result
 
 obj = PlantAPI('TREFLE')
 data = obj.get_plants('595', 'white', 'green', 'blue')
-# print(list(data.columns.values))
+
 
 # Contains basic info on plants filtered by API call
-df = data[['id', 'common_name', 'image_url']].copy()
+df = data[['id', 'common_name']].copy()
 # print(df)
 
 # Individual plant information
-plant = obj.get_indiv_plant()
-# print(plant)
+plant = obj.get_indiv_plant('158178')
+clean_plant = obj.transform(plant)
+result = obj.json_result(clean_plant)
+print(result)
 
