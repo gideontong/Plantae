708c8386b171fde3f9db215e56befe4ebee3fbb2
Gideon Tong <gideon@gideontong.com>
2020-10-08 02:10:12 -0700

    Set up Flask and API templates

--- server/app.py
+++ server/app.py
@@ -2,5 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return '{"about":"Plantae API"}'
+
+@app.route('/search')
+def search():
+    '''
+    Returns a JSON object with the number of results and a key to repeat the same search or get results.
+    '''
+    return '{"results":0,"key":0}'
+
+@app.route('/results')
+def search_results():
+    '''
+    Returns a JSON object with the results given a search key.
+    '''
+    return '{"results":[]}'
+
+@app.route('/plant')
+def get_plant():
+    '''
+    Returns a JSON object of the first plant in the search results or a specific plant given a key.
+    '''
+    return '{"plant":{}}'
