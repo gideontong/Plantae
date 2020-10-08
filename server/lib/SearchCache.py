5de52a293dd82f254f61b3bd039341c82820ddc0
Gideon Tong <gideon@gideontong.com>
2020-10-08 02:20:07 -0700

    Complete function definitions of search cache

--- server/lib/SearchCache.py
+++ server/lib/SearchCache.py
@@ -2,33 +2,41 @@
 Caches responses from the Trefle API.
 '''
 
+from collections import OrderedDict as better_dict
+
 class SearchCache:
-    def __init__(self):
-        self.cache = {}
+    def __init__(self, max_size: int):
+        self.cache = better_dict()
+        self.max_size = max_size
 
     def add(self, key: str, data: dict) -> int:
         '''
         Adds an entry to the cache. Returns size of cache.
         '''
-        pass
+        self.auto_evict()
+        self.cache[key] = data
 
     def get(self, key: str) -> dict:
         '''
         Gets an item from the cache.
         '''
-        pass
+        return self.cache[key]
 
     def evict(self, key: str) -> dict:
         '''
         Evicts an entry from the cache. Returns the evicted entry.
         '''
-        pass
+        del self.cache[key]
 
-    def auto_evict(self, key: str) -> int:
+    def auto_evict(self) -> int:
         '''
         Evicts old entries from the cache with a condition is hit. Returns number of entries evicted.
         '''
-        pass
+        count = 0
+        while len(self.cache) > self.max_size:
+            self.cache.popitem(last=False)
+            count += 1
+        return count
 
     def size(self) -> int:
         '''
