6624a2242dd79c0bc16ae0ca10b8cd0b2bb398a5
devyboy <dev@udel.edu>
2020-10-09 11:17:20 -0400

    actual search page form functional

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Link } from 'gatsby';
-import { Typography } from 'antd';
+import { Typography, Divider } from 'antd';
+import SearchForm from '../components/searchForm';
 
 const SearchPage = () => {
   const { Title, Text } = Typography;
+
   return (
     <Layout>
       <Title level={1}>Search</Title>
@@ -12,7 +14,14 @@ const SearchPage = () => {
         Don't know what you're looking for? Try{' '}
         <Link to='/id'>identifying</Link> instead.
       </Text>
+      <Divider />
+      <Title level={4}>
+        Enter a keyword related to your plant species below and let us do all
+        the work.
+      </Title>
+      <SearchForm />
     </Layout>
   );
 };
+
 export default SearchPage;
