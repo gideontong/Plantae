f58082edc80bba7e56a8c9bf9a83cd1b28088299
devyboy <dev@udel.edu>
2020-10-10 18:22:25 -0400

    remove unused packages, small changes

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+  if (page.path.match(/^\/plant/)) {
+    page.matchPath = '/plant/*';
+    createPage(page);
+  }
+};
